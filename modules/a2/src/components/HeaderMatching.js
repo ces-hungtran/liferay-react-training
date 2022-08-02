8122445af97825f7b915d907c1c2c04227ba9e3e
Tran Minh-Hung <hung.tran@codeenginestudio.com>
2022-08-02 11:50:33 +0700

    init project.

---
+++ modules/a2/src/components/HeaderMatching.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class HeaderMatching extends React.Component {
+  render() {
+    return <h1>header matching {this.props}</h1>;
+  }
+}
+
+export default HeaderMatching;
--- modules/a2/src/components/UploadFile.js
+++ modules/a2/src/components/UploadFile.js
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "@mui/material/Button";
 
 import { Formik } from "formik";
+import HeaderMatching from "./HeaderMatching";
 
 class UploadFile extends React.Component {
   constructor(props) {
@@ -9,75 +10,77 @@ class UploadFile extends React.Component {
     this.state = { file: "" };
   }
   renderFileContent = () => {
-    return <h1>{this.state.fileContent}</h1>;
+    return <h1>xxx</h1>;
+    // return <HeaderMatching {...this.state} />;
   };
+
   render() {
-    if (this.state.loading) {
-      return <h1>Loading...</h1>;
-    }
-    if (this.state.fileContent) {
-      return this.renderFileContent();
-    }
-    const formikProps = {
-      initialValues: {
-        uploadCSV: "",
-        uploadCSVInputBox: "",
-      },
-      validateOnBlur: true,
-      validateOnchange: true,
+    // if (this.state.loading) {
+    return <h1>Loading...</h1>;
+    // }
+    // if (this.state.fileContent) {
+    //   return this.renderFileContent();
+    // }
+    // const formikProps = {
+    //   initialValues: {
+    //     uploadCSV: "",
+    //     uploadCSVInputBox: "",
+    //   },
+    //   validateOnBlur: true,
+    //   validateOnchange: true,
 
-      onSubmit: async (values, action) => {
-        console.log("values", values);
-        this.setState({ file: values["filehxx"] });
+    //   onSubmit: async (values, action) => {
+    //     console.log("values", values);
+    //     this.setState({ file: values["filehxx"] });
 
-        this.setState({ loading: true }, () => {
-          const reader = new FileReader();
-          reader.onloadend = () => {
-            this.setState({
-              loading: false,
-              fileContent: reader.result,
-            });
-          };
-          reader.readAsText(values["filehxx"]);
-        });
-      },
-    };
+    //     this.setState({ loading: true }, () => {
+    //       const reader = new FileReader();
+    //       reader.onloadend = () => {
+    //         this.setState({
+    //           loading: false,
+    //           fileContent: reader.result,
+    //         });
+    //       };
+    //       reader.readAsText(values["filehxx"]);
+    //     });
+    //   },
+    // };
 
-    return (
-      <Formik {...formikProps}>
-        {(props) => {
-          return (
-            <>
-              <form>
-                <Button id="uploadCSV" variant="contained" component="label">
-                  Upload CSV file
-                  <input
-                    value={props.values["uploadCSVInputBox"]}
-                    onChange={(event) => {
-                      console.log("hxx file", event.currentTarget.files[0]);
-                      props.setFieldValue(
-                        "filehxx",
-                        event.currentTarget.files[0]
-                      );
-                      props.handleChange(event);
-                    }}
-                    id="uploadCSVInputBox"
-                    hidden
-                    accept=".csv"
-                    multiple
-                    type="file"
-                  />
-                </Button>
+    // return (
+    //   <Formik {...formikProps}>
+    //     {(props) => {
+    //       return (
+    //         <>
+    //           <form>
+    //             <Button id="uploadCSV" variant="contained" component="label">
+    //               Upload CSV file
+    //               <input
+    //                 value={props.values["uploadCSVInputBox"]}
+    //                 onChange={(event) => {
+    //                   console.log("hxx file", event.currentTarget.files[0]);
+    //                   props.setFieldValue(
+    //                     "filehxx",
+    //                     event.currentTarget.files[0]
+    //                   );
+    //                   props.handleChange(event);
+    //                 }}
+    //                 id="uploadCSVInputBox"
+    //                 hidden
+    //                 accept=".csv"
+    //                 multiple
+    //                 type="file"
+    //               />
+    //             </Button>
 
-                <Button onClick={props.handleSubmit} type="submit">
-                  submit hxx
-                </Button>
-              </form>
-            </>
-          );
-        }}
-      </Formik>
-    );
+    //             <Button onClick={props.handleSubmit} type="submit">
+    //               submit hxx
+    //             </Button>
+    //           </form>
+    //         </>
+    //       );
+    //     }}
+    //   </Formik>
+    // );
   }
 }
 
